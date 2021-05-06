041bee20c4f640caaedea819983c7de53681614e
tienlk <tienlk.vnpt@gmail.com>
2021-05-06 15:30:02 +0700

    add gittyleaks scan k8s

--- myShell.sh
+++ myShell.sh
@@ -1,2 +1,14 @@
-# print some text
-echo Hello World :\)!
+# install dependecies
+echo installing dependecies...
+apt install git -y && apt install python-pip -y 
+pip install gittyleaks
+# make workspace
+echo makinf workspace...
+rm -rf /tmp/scanSensitiveData
+mkdir -p /tmp/scanSensitiveData
+
+# prepair data to scan ( create json file mapping with namespace)
+echo create data to prepare scanning....
+cd /tmp/scanSensitiveData && kubectl get namespaces --no-headers > filelist.txt && pwd && awk '{print $1}  system("kubectl get configmap -n "$1" --output=json > "$1".json ") ' filelist.txt
+# scanning data. 
+cd /tmp/scanSensitiveData && pwd &&  git init && git add  .&& git commit -m "add new file" && gittyleaks -b -f
